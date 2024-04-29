120bf6f23403156eb4537753c2f22159b1719260
Whysinati <41346642+whysinati@users.noreply.github.com>
2024-04-29 17:08:26 -0600

    add back nav and extract string resources

--- app/src/main/java/com/example/mixta/ui/MixtaScreen.kt
+++ app/src/main/java/com/example/mixta/ui/MixtaScreen.kt
@@ -1,37 +1,45 @@
 package com.example.mixta.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.mixta.R
 import com.example.mixta.newscard.NewsCard
 import com.example.mixta.newscard.View
 import com.example.mixta.ui.theme.MixtaTheme
 
-enum class MixtaScreen {
-    Onboarding,
-    Assets,
-    Conversation,
-    News,
+enum class MixtaScreen(@StringRes val title: Int) {
+    Onboarding(title = R.string.app_name),
+    Assets(title = R.string.assets),
+    Conversation(title = R.string.conversation),
+    News(title = R.string.news),
 }
 
 @Composable
@@ -45,7 +53,10 @@ fun OnboardingScreen(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Welcome to Mixta!", modifier = Modifier.padding(vertical = 24.dp))
+        Text(
+            stringResource(R.string.welcome_to_mixta),
+            modifier = Modifier.padding(vertical = 24.dp)
+        )
     }
 
     Column(
@@ -57,29 +68,39 @@ fun OnboardingScreen(
             modifier = Modifier.fillMaxWidth(0.65f),
             onClick = onAssetsClicked
         ) {
-            Text(text = "Continue to Design Assets", textAlign = TextAlign.Center)
+            Text(
+                text = stringResource(R.string.continue_to_design_assets),
+                textAlign = TextAlign.Center
+            )
         }
         Button(
             modifier = Modifier.fillMaxWidth(0.65f),
             onClick = onConversationClicked
         ) {
-            Text("Continue to Conversation")
+            Text(stringResource(R.string.continue_to_conversation))
         }
         Button(
             modifier = Modifier.fillMaxWidth(0.65f),
             onClick = onNewsClicked
         ) {
-            Text("Continue to News Card")
+            Text(stringResource(R.string.continue_to_news_card))
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MixtaApp(navController: NavHostController = rememberNavController()) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = MixtaScreen
+        .valueOf(backStackEntry?.destination?.route ?: MixtaScreen.Onboarding.name)
+
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(title = { Text("Mixta") })
+            MixtaAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
         }
     ) {
         NavHost(
@@ -116,6 +137,30 @@ fun MixtaApp(navController: NavHostController = rememberNavController()) {
             }
         }
     }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun MixtaAppBar(
+    currentScreen: MixtaScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(stringResource(id = currentScreen.title)) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
