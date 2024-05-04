526595a8ea9a96b9aafd11e2ae2dd25611ca17a5
Whysinati <41346642+whysinati@users.noreply.github.com>
2024-05-04 03:41:11 -0600

    remove unfinished android test

--- app/src/androidTest/java/com/example/mixta/NavigationTest.kt
+++ app/src/androidTest/java/com/example/mixta/NavigationTest.kt
@@ -2,24 +2,24 @@ package com.example.mixta
 
 class NavigationTest {
 
-    @get:Rule
-    val composeTestRule = createComposeRule()
-    lateinit var navController: TestNavHostController
-
-    @Before
-    fun setupAppNavHost() {
-        composeTestRule.setContent {
-            navController = TestNavHostController(LocalContext.current)
-            navController.navigatorProvider.addNavigator(ComposeNavigator())
-            AppNavHost(navController = navController)
-        }
-    }
-
-    // Unit test
-    @Test
-    fun appNavHost_verifyStartDestination() {
-        composeTestRule
-            .onNodeWithContentDescription("Start Screen")
-            .assertIsDisplayed()
-    }
+//    @get:Rule
+//    val composeTestRule = createComposeRule()
+//    lateinit var navController: TestNavHostController
+//
+//    @Before
+//    fun setupAppNavHost() {
+//        composeTestRule.setContent {
+//            navController = TestNavHostController(LocalContext.current)
+//            navController.navigatorProvider.addNavigator(ComposeNavigator())
+//            AppNavHost(navController = navController)
+//        }
+//    }
+//
+//    // Unit test
+//    @Test
+//    fun appNavHost_verifyStartDestination() {
+//        composeTestRule
+//            .onNodeWithContentDescription("Start Screen")
+//            .assertIsDisplayed()
+//    }
 }
